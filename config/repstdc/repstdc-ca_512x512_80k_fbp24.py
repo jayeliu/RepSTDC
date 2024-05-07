ba3b1959e20e6a0b3bdc35fc06e125eeb40446e0
Jae Liu <34577583+jayeliu@users.noreply.github.com>
2024-05-08 00:00:49 +0800

    add repstdc config

---
+++ config/repstdc/repstdc-ca_512x512_80k_fbp24.py
@@ -0,0 +1,8 @@
+_base_=['./repstdc_512x512_80k_fbp24.py']
+crop_size = (512,512)
+data_preprocessor = dict(size=crop_size)
+model = dict(data_preprocessor=data_preprocessor,
+             backbone=dict(
+                 fusion_type="CA",
+             )
+             )
