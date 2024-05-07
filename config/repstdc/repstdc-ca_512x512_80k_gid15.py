5da36a9d27a92bddccb7c8c9f7e3e3cad6264408
Jae Liu <34577583+jayeliu@users.noreply.github.com>
2024-05-08 00:01:47 +0800

    Delete config directory

--- config/repstdc/repstdc-ca_512x512_80k_gid15.py
+++
@@ -1,8 +0,0 @@
-_base_=['./repstdc_512x512_80k_gid15.py']
-crop_size = (512,512)
-data_preprocessor = dict(size=crop_size)
-model = dict(data_preprocessor=data_preprocessor,
-             backbone=dict(
-                 fusion_type="CA",
-             )
-             )
--- config/repstdc/repstdc-eca-ca_512x512_80k_gid5.py
+++
@@ -1,9 +0,0 @@
-_base_=['./repstdc_512x512_80k_gid5.py']
-crop_size = (512,512)
-data_preprocessor = dict(size=crop_size)
-model = dict(data_preprocessor=data_preprocessor,
-             backbone=dict(
-                 fusion_type="CA",
-                 refine_type="ECA"
-             )
-             )
